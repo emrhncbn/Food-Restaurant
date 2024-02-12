ed72d4c41fc812da3241ede3de9b2aa70c52412a
emrhncbn <emrhncbn@gmail.com>
2024-02-12 13:28:23 +0300

    Food Restaurant Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import './styles.css'
+
+
+
+const App = () => {
+  return (
+    <div>
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+  )
+}
+export default App
+/* Challenge: 
+
+Bu web sayfası, HTML öğeleri vanilla JS ile DOM'a eklenerek oluşturulmuştur. Göreviniz JS'yi React JSX'e dönüştürmek ve React ile sayfaya render etmek. İşte istediğimiz şeyler: 
+
+        1. Sayfa, JSX ile yazılmış diğer fonksiyonel bileşenlerden ve elementlerden oluşan tek bir fonksiyonel React bileşeni tarafından render edilmelidir.
+        
+        2. Sonuç, mevcut sayfadan farklı olmamalıdır (bonus challenge yapmadığınız sürece).
+        
+        3. Mevcut JS'nin tamamı sonunda silinmelidir. 
+        
+        4. Kod modüler ve iyi organize edilmiş olmalıdır.
+*/
+
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+  return (
+    <header>
+     <div class="button-container">
+      <button>Menu</button> 
+      <button>Hakkında</button> 
+      <button>Saatler & Lokasyon</button>
+     </div> 
+    </header>
+  )
+}
+export default Header
